3d7588b63bdf799ccd8eaaed5ef958617e41f0c2
Lucas Guimaro <106471648+LukasGuimaro@users.noreply.github.com>
2024-11-03 23:41:05 -0400

    publico

---
+++ publico/index.php
@@ -0,0 +1,21 @@
+<?php 
+if(!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER["PATH_INFO"] == "/") {
+    require_once(__DIR__."/../pagina-inicial.php");
+}
+else if ($_SERVER["PATH_INFO"] == "/admin"){
+    require_once(__DIR__."/../admin.php");
+}
+else if ($_SERVER["PATH_INFO"] == "/cadastrar"){
+    require_once(__DIR__."/../cadastrar-produto.php");
+}
+else if ($_SERVER["PATH_INFO"] == "/excluir"){
+    require_once(__DIR__."/../excluir-produto.php");
+}
+else if ($_SERVER["PATH_INFO"] == "/editar"){
+    require_once(__DIR__."/../editar-produto.php");
+}
+else if ($_SERVER["PATH_INFO"] == "/relatorio"){
+    require_once(__DIR__."/../gerador-pdf.php");
+}
+
+?>
